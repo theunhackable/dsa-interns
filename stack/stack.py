f96cb0eb164866785ff2814eafb93a3570347ea1
theunhackable <srirangasaipagala@gmail.com>
2024-07-30 22:05:34 +0530

    stack skeleton

---
+++ stack/stack.py
@@ -0,0 +1,18 @@
+class Node:
+  def __init__(self, val, next=None):
+    self.val = val
+    self.next = next
+
+
+class Stack:
+  def __init__(self, top=None):
+    self.top = top
+  def push(self, val):
+    #add element to the top
+    pass
+  def pop(self):
+    # remove top element
+    pass
+  def peek(self):
+    #return the top element
+    pass
