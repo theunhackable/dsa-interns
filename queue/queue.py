f6c9e23df3870c698a386d41eae582501bdf89cd
Pranjal Prince <pranjalprince522001@gmail.com>
2024-08-06 20:31:21 +0530

    modifying queue code

--- queue/queue.py
+++ queue/queue.py
@@ -1,19 +1,40 @@
 class Node:
-  def __init__(self, val, next=None):
-    self.val = val
-    self.next = next
-
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
 
 class Queue:
-  def __init__(self, front=None, back=None):
-    self.front = front
-    self.back = back
-  def push(self, val):
-    #add element to the end of the ll
-    pass
-  def pop(self):
-    # remove first element from the ll
-    pass
-  def peek(self):
-    #return the first element
-    pass
+    def __init__(self):
+        self.front = None
+        self.back = None
+
+    def push(self, val):
+        new_node = Node(val)
+        if self.back is None:
+            # If the queue is empty, both front and back are the new node
+            self.front = self.back = new_node
+        else:
+            # Add the new node at the end and update back
+            self.back.next = new_node
+            self.back = new_node
+
+    def pop(self):
+        if self.front is None:
+            raise IndexError("pop from empty queue")
+        
+        # Remove the front node
+        removed_value = self.front.val
+        self.front = self.front.next
+        
+        if self.front is None:
+            # If the queue is empty now, update back to None
+            self.back = None
+        
+        return removed_value
+
+    def peek(self):
+        if self.front is None:
+            raise IndexError("peek from empty queue")
+        
+        # Return the value of the front node
+        return self.front.val
