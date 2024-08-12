4f71a3279d2f42a08c2e64dc3a1b376adc17fda4
theunhackable <srirangasaipagala@gmail.com>
2024-08-12 21:49:52 +0530

    added trees

---
+++ binary-trees/binary_trees.py
@@ -0,0 +1,11 @@
+'''
+Node:
+  value
+  left, right
+'''
+
+class Node:
+  def __init__(self, val, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
--- queue/queue.py
+++ queue/queue.py
@@ -10,7 +10,14 @@ class Queue:
     self.back = back
   def push(self, val):
     #add element to the end of the ll
-    pass
+    new_node = Node(val)
+    if self.back == None:
+      self.front = new_node
+      self.back = new_node  
+    else:
+      new_node.next = self.back 
+      self.back = new_node 
+    
   def pop(self):
     # remove first element from the ll
     pass
