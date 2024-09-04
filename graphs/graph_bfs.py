949afbb68a25037d8ebac3eb58a51208e4a7d5a8
theunhackable <srirangasaipagala@gmail.com>
2024-09-04 22:08:53 +0530

    corrected BFS

--- graphs/graph_bfs.py
+++ graphs/graph_bfs.py
@@ -6,11 +6,12 @@ from collections import deque
     b) check if the poped out if visited[node] == false
         i) mark it as true.
         ii) add unvisited neighbours to the queue
-        iii) print the popped node 
+        iii) print the popped node
 
 '''
 
-adj_list = [[1,6,5], [0, 2], [1,3,4,8], [2, 4, 7], [2, 3], [0, 6], [0, 5], [3], [2]]
+adj_list = [[1, 6, 5], [0, 2], [1, 3, 4, 8], [
+    2, 4, 7], [2, 3], [0, 6], [0, 5], [3], [2]]
 
 n = len(adj_list)
 # visited array
@@ -19,13 +20,12 @@ visited = [False] * n
 q = deque()
 
 q.append(0)
-
+visited[0] = True
 while len(q):
-  node = q.popleft()
-  if visited[node] == False:
-    visited[node] = True
+    node = q.popleft()
     for neighbour in adj_list[node]:
-      if visited[neighbour] == False:
-        q.append(neighbour)
-    print(node, end=" ")
+        if not visited[neighbour]:
+            q.append(neighbour)
+            visited[neighbour] = True
 
+    print(node, end=" ")
