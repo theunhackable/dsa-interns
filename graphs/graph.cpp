211eb60f3027d467f02221cf1ab8882491f4a2a1
theunhackable <srirangasaipagala@gmail.com>
2024-09-25 22:05:17 +0530

    cpp code for vectors

---
+++ graphs/graph.cpp
@@ -0,0 +1,23 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    // Write C++ code here
+    printf("Try programiz.pro");
+    vector<int> a;
+
+    return 0;
+}
+
+/*
+vector<int> a;  -> {}
+
+vector<vector<int>> mat: -> {}
+
+mat.push_back(a);
+
+//  in physics
+velocity = [x, y, z]
+
+*/
